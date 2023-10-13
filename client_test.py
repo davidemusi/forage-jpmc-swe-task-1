4b4668b00d3542cf73d45ecf29aa5f29d729629c
davidemusi <davidemusi4gospel@gmail.com>
2023-10-13 13:28:32 +0100

    Add updated getRatio and getDataPoint methods

--- client_test.py
+++ client_test.py
@@ -8,6 +8,11 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    stock, bid_price, ask_price, price = getDataPoint(quotes[0])
+    self.assertEqual(stock, 'ABC')
+    self.assertEqual(bid_price, 120.48)
+    self.assertEqual(ask_price, 121.2)
+    self.assertEqual(price, (120.48 + 121.2) / 2)
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,11 +20,30 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    stock, bid_price, ask_price, price = getDataPoint(quotes[1])
+    self.assertEqual(stock, 'DEF')
+    self.assertEqual(bid_price, 117.87)
+    self.assertEqual(ask_price, 121.68)
+    self.assertEqual(price, (117.87 + 121.68) / 2)
 
 
   """ ------------ Add more unit tests ------------ """
+  def test_getDataPoint_calculatePriceEqualBidAndAsk(self):
+    quotes = [
+      {'top_ask': {'price': 120.0, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.0, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 122.0, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 122.0, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'},
+      {'top_ask': {'price': 123.0, 'size': 6}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 123.0, 'size': 91}, 'id': '0.109974697771', 'stock': 'GHI'}
+    ]
+    
+    stock, bid_price, ask_price, price = getDataPoint(quotes[2])
+    self.assertEqual(stock, 'GHI')
+    self.assertEqual(bid_price, 123.0)
+    self.assertEqual(ask_price, 123.0)
+    self.assertEqual(price, 123.0)
 
 
 
 if __name__ == '__main__':
     unittest.main()
+
+
